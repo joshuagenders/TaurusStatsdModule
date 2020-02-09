1b944ae503f59e845779bd8f959f577b07f7f615
Joshua.Genders <joshuagenders@hotmail.com>
2020-02-09 12:44:30 +0000

    parse and send metrics

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,4 @@ COPY ./tests.yml ./tests.yml
 COPY ./statsd_reporter.py /usr/local/lib/python2.7/dist-packages/bzt/modules/statsd_reporter.py
 COPY ./base-config.yml /bzt-configs/base-config.yml
 
-ENTRYPOINT ["bash"]
+ENTRYPOINT ["bzt", "tests.yml"]
--- statsd_reporter.py
+++ statsd_reporter.py
@@ -1,5 +1,5 @@
 from bzt.engine import Reporter, Singletone
-from bzt.modules.aggregator import DataPoint, AggregatorListener, ResultsProvider
+from bzt.modules.aggregator import DataPoint, KPISet, AggregatorListener, ResultsProvider
 import statsd, os, json
 
 class StatsdReporter(Reporter, AggregatorListener, Singletone):
@@ -8,14 +8,15 @@ class StatsdReporter(Reporter, AggregatorListener, Singletone):
         self.data_buffer = []
 
     def aggregated_second(self, data):
-        self.data_buffer += data
+        self.data_buffer.append(data)
 
     def prepare(self):
         super(StatsdReporter, self).prepare()
+        self.engine.aggregator.add_listener(self)
 
     def check(self):
         for point in self.data_buffer:
-            self.serialiser.serialise(point)           
+            self.serialiser.serialise(point)
         self.data_buffer = []
 
     def startup(self):
@@ -28,10 +29,15 @@ class DataSerialiser():
     def __init__(self):
         host = os.getenv("STATSD_HOST", "localhost")
         port = int(os.getenv("STATSD_PORT", "8125"))
-        self.client = statsd.StatsClient(host, port)
+        prefix = os.getenv("STATSD_PREFIX", "taurus.unset")
+        self.client = statsd.StatsClient(host, port, prefix)
 
-    def serialise(self, datapoint):
-        # todo parse and push datapoints
-        output = json.dumps(datapoint)
-        print(output)
-        
+    def serialise(self, data):
+        cur = data[DataPoint.CURRENT].get('')
+        self.client.gauge('concurrency', cur[KPISet.CONCURRENCY])
+        self.client.gauge('errors', cur[KPISet.FAILURES])
+        self.client.gauge('success', cur[KPISet.SUCCESSES])
+        self.client.gauge('total', cur[KPISet.SAMPLE_COUNT])
+        self.client.timer('response', cur[KPISet.AVG_RESP_TIME])
+        self.client.timer('latency', cur[KPISet.AVG_LATENCY])
+        self.client.timer('connect', cur[KPISet.AVG_CONN_TIME])
